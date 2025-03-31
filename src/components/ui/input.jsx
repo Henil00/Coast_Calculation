673a793e7de160d66759b7a85f15f5dc8a3a9eec
Henil00 <henilpatel11111975@gmail.com>
2025-03-31 20:26:56 +0530

    Added Black font

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -26,7 +26,7 @@ const Input = ({ label, id, value, onChange,required, ...props }) => {
   />
   <label
     htmlFor={id}
-    className="absolute text-lg font-bold text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-3 origin-[0] bg-white dark:bg-gray-dark px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-3 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+    className="absolute text-lg font-bold text-black dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-3 origin-[0] bg-white dark:bg-gray-dark px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-3 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
   >
     {label}
   </label>
